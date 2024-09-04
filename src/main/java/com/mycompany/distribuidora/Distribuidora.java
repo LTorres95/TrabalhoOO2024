19a15b4a8551bfbd9727f1726900f2db0c1dcb12
Lucas Torres <97073346+LTorres95@users.noreply.github.com>
2024-09-04 17:37:49 -0300

    Primeiro Commit

---
+++ src/main/java/com/mycompany/distribuidora/Distribuidora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.distribuidora;
+
+/**
+ *
+ * @author ltmar
+ */
+public class Distribuidora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
