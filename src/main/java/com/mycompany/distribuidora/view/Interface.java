441b6973999cb5a0f0e98496704f86ebad07cb85
joao-nilson <jnilson@ice.ufjf.br>
2024-09-08 16:43:38 -0300

    inicia jFrame e desenha na tela

--- src/main/java/com/mycompany/distribuidora/view/Interface.java
+++ src/main/java/com/mycompany/distribuidora/view/Interface.java
@@ -15,5 +15,25 @@ import java.util.List;
  */
 public class Interface {
 
+    private JFrame tela;
+    private final int WIDTH = 500;
+    private final int HEIGHT = 200;
+    private final int V_GAP = 10;
+    private final int H_GAP = 5;
+    
+    private JTextField tfNome;
+    private JTextField tfEmail;
+    private JTextField tfTelefone;
+    
+    public void desenha() {
 
+        tela = new JFrame("Distribuidora");
+        tela.setSize(WIDTH, HEIGHT);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tela.setVisible(true);
+        tela.setLayout(new BorderLayout());
+
+
+        tela.pack();
+    }
 }
