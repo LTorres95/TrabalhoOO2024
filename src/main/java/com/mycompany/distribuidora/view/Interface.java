9948400655320e637216e1b8374592290ba60a0a
joao-nilson <jnilson@ice.ufjf.br>
2024-09-09 09:16:50 -0300

    desenha prototipo da tela inicial

--- src/main/java/com/mycompany/distribuidora/view/Interface.java
+++ src/main/java/com/mycompany/distribuidora/view/Interface.java
@@ -6,7 +6,9 @@ package com.mycompany.distribuidora.view;
 
 
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 /**
@@ -17,23 +19,43 @@ public class Interface {
 
     private JFrame tela;
     private final int WIDTH = 500;
-    private final int HEIGHT = 200;
+    private final int HEIGHT = 500;
     private final int V_GAP = 10;
     private final int H_GAP = 5;
     
     private JTextField tfNome;
+    private JTextField tfCpf;
+    private JTextField tfCnpj;
     private JTextField tfEmail;
     private JTextField tfTelefone;
+    private JTextField tfSenha;
     
     public void desenha() {
 
         tela = new JFrame("Distribuidora");
+        tela.setVisible(true);
         tela.setSize(WIDTH, HEIGHT);
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tela.setVisible(true);
+        tela.setLocationRelativeTo(null);
         tela.setLayout(new BorderLayout());
-
-
+        
+        desenhaTelaInicial();
+        
         tela.pack();
+    }
+    
+    private void desenhaTelaInicial(){
+        JPanel painel = new JPanel();
+        painel.setBorder(BorderFactory.createTitledBorder("Tela inicial"));
+        
+        JButton btnLogin = new JButton("Login");
+        JButton btnCadastro = new JButton("Cadastro");
+
+        JPanel botoes = new JPanel();
+        botoes.add(btnLogin);
+        botoes.add(btnCadastro);
+        
+        painel.add(botoes, BorderLayout.SOUTH);
+        tela.getContentPane().add(painel, BorderLayout.CENTER);
     }
 }
