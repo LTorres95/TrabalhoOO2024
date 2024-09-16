3f8eeefdcf7698b722fba7ec30f2b866502686b7
joao-nilson <jnilson@ice.ufjf.br>
2024-09-16 02:40:20 -0300

    implementa metodo save usando gson

--- src/main/java/com/mycompany/distribuidora/persistencia/CadastroPersistencia.java
+++ src/main/java/com/mycompany/distribuidora/persistencia/CadastroPersistencia.java
@@ -21,7 +21,14 @@ public class CadastroPersistencia implements Persistencia<Cadastro>  {
     private static final String PATH = DIRECTORY+ File.separator +"cadastros.json";
     @Override
     public void save(List<Cadastro> itens) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Gson gson = new Gson();
+        String json = gson.toJson(itens);
+
+        File diretorio = new File(DIRECTORY);
+        if(!diretorio.exists())
+            diretorio.mkdirs();
+
+        Arquivo.salva(PATH, json);
     }
 
     @Override
