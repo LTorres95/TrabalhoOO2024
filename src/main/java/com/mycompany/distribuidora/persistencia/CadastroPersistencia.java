37e9d1096857395d2584d0c8825b44a7c4a2d7f3
joao-nilson <jnilson@ice.ufjf.br>
2024-09-16 02:31:59 -0300

    adiciona caminho para salvar dados e metodos save e findAll

--- src/main/java/com/mycompany/distribuidora/persistencia/CadastroPersistencia.java
+++ src/main/java/com/mycompany/distribuidora/persistencia/CadastroPersistencia.java
@@ -3,11 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.distribuidora.persistencia;
+import com.mycompany.distribuidora.model.Cadastro;
 
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
 /**
  *
  * @author si
  */
-public class CadastroPersistencia {
+public class CadastroPersistencia implements Persistencia<Cadastro>  {
+
+    private static final String PATH = DIRECTORY+ File.separator +"cadastros.json";
+    @Override
+    public void save(List<Cadastro> itens) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Cadastro> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
