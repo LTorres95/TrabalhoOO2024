8616d1fc3a6226b0d5ed3c0e9f9471aafe454fc8
joao-nilson <jnilson@ice.ufjf.br>
2024-09-28 20:58:41 -0300

    implementa lista de lotes e produtos para vizualizacao

--- src/main/java/com/mycompany/distribuidora/view/InterfaceEstoque.java
+++ src/main/java/com/mycompany/distribuidora/view/InterfaceEstoque.java
@@ -6,6 +6,7 @@ package com.mycompany.distribuidora.view;
 
 import com.mycompany.distribuidora.model.Estoque.Produto;
 import com.mycompany.distribuidora.model.Estoque.Estoque;
+import com.mycompany.distribuidora.model.Estoque.Lote;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,7 +20,8 @@ import java.util.List;
 public class InterfaceEstoque {
     private JFrame frame;
     private JList<Produto> produtoList;
-    private DefaultListModel<Produto> listModel;
+    private JList<Lote> loteList;
+    private DefaultListModel<Lote> loteListModel;
     private Estoque estoque;
     
     public InterfaceEstoque(Estoque estoque){
@@ -29,47 +31,71 @@ public class InterfaceEstoque {
 
     private void inicializa() {
         frame = new JFrame("Estoque de produtos");
-        frame.setSize(400, 300);
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         
-        listModel = new DefaultListModel<>();
-        produtoList = new JList<>(listModel);
+        DefaultListModel<Produto> produtoListModel = new DefaultListModel<>();
+        produtoList = new JList<>(produtoListModel);
         produtoList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        produtoList.addListSelectionListener(e -> carregarLotes());
         
-        carregarProdutos();
         
-        JScrollPane scrollPane = new JScrollPane(produtoList);
-        frame.add(scrollPane, BorderLayout.CENTER);
+        loteListModel = new DefaultListModel<>();
+        loteList = new JList<>(loteListModel);
+        loteList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        carregarProdutos(produtoListModel);
+        
+        JScrollPane produtoScrollPane = new JScrollPane(produtoList);
+        JScrollPane loteScrollPane = new JScrollPane(loteList);
+//        frame.add(scrollPane, BorderLayout.CENTER);
+        
+        JPanel controlPanel = new JPanel(new GridLayout(1, 2));
+        controlPanel.add(produtoScrollPane);
+        controlPanel.add(loteScrollPane);
+        
         
         JPanel botoesPainel = new JPanel();
         JButton btnDetalhes = new JButton("Ver detalhes");
         btnDetalhes.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                Produto produtoSelecionado = produtoList.getSelectedValue();
-                if (produtoSelecionado != null)
-                    mostrarDetalhes(produtoSelecionado);
+                Lote loteSelecionado = loteList.getSelectedValue();
+                if (loteSelecionado != null)
+                    mostrarDetalhes(loteSelecionado);
                 else
-                    JOptionPane.showMessageDialog(frame, "Selecione um produto.");
+                    JOptionPane.showMessageDialog(frame, "Selecione um lote.");
             }
         });
         botoesPainel.add(btnDetalhes);
         
+        frame.add(controlPanel, BorderLayout.CENTER);
         frame.add(botoesPainel, BorderLayout.SOUTH);
+        
         frame.setVisible(true);
     }
 
-    private void carregarProdutos() {
-        listModel.clear();
-        List<Produto> produtos = estoque.getProdutos();
-        for(Produto produto : produtos)
-            listModel.addElement(produto);
+    private void carregarProdutos(DefaultListModel<Produto> produtoListModel) {
+        loteListModel.clear();
+        for(Produto produto : estoque.getProdutos())
+            produtoListModel.addElement(produto);
     }
     
-    private void mostrarDetalhes(Produto produto){
-        String detalhes = String.format("Nome: %s\nCódigo: %d\nPreço: %.2f\nQuantidade: %d",
-                produto.getNome(), produto.getCodigo(), produto.getPreco(), produto.getQuantidade());
+    private void carregarLotes() {
+        loteListModel.clear();
+        Produto produtoSelecionado = produtoList.getSelectedValue();
+        if (produtoSelecionado != null) {
+            List<Lote> lotes = produtoSelecionado.getLotes();
+            for (Lote lote : lotes) {
+                loteListModel.addElement(lote);
+            }
+        }
+    }
+    
+    private void mostrarDetalhes(Lote lote){
+        String detalhes = String.format("Código do Lote: %d\nQuantidade: %d\nValidade: %s",
+                lote.getCodigoLote(), lote.getQuantidade(), lote.getValidade());
         JOptionPane.showMessageDialog(frame, detalhes, "Detalhes do Produto", JOptionPane.INFORMATION_MESSAGE);
 
     }
