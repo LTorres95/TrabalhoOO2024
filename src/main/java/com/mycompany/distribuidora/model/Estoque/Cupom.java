151afcf78fee033718a4958cf465e87bd87cf8d6
bighat-logan-ds1 <86729981+bighat-logan-ds1@users.noreply.github.com>
2024-09-29 14:38:25 -0300

    Update Cupom.java

--- src/main/java/com/mycompany/distribuidora/model/Estoque/Cupom.java
+++ src/main/java/com/mycompany/distribuidora/model/Estoque/Cupom.java
@@ -4,13 +4,14 @@ public class Cupom extends Promocao{
     
     protected int codigoCupom;
     
+    public int getCodigoCupom(){return codigoCupom;}
     Cupom(int codigoProduto,double novoValor,int codigoCupom)
     {
         super(codigoProduto,novoValor);
         this.codigoCupom=codigoCupom;
     }
 
-    public int getCodigoCupom(){return codigoCupom;}
+    
     
     
 }
