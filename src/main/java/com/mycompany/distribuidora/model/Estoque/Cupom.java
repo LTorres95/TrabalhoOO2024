77fd243d02c6bce54bb89965126611cc89386c90
bighat-logan-ds1 <86729981+bighat-logan-ds1@users.noreply.github.com>
2024-09-29 14:39:57 -0300

    Update Cupom.java

--- src/main/java/com/mycompany/distribuidora/model/Estoque/Cupom.java
+++ src/main/java/com/mycompany/distribuidora/model/Estoque/Cupom.java
@@ -1,17 +1,26 @@
 package com.mycompany.distribuidora.model.Estoque;
 
-public class Cupom extends Promocao{
-    
+import com.mycompany.distribuidora.model.Estoque.exceptions.CupomException;
+import com.mycompany.distribuidora.model.Estoque.exceptions.PromocaoException;
+
+public class Cupom extends Promocao {
+
     protected int codigoCupom;
-    
-    public int getCodigoCupom(){return codigoCupom;}
-    Cupom(int codigoProduto,double novoValor,int codigoCupom)
-    {
-        super(codigoProduto,novoValor);
-        this.codigoCupom=codigoCupom;
+
+    Cupom(int codigoProduto, double novoValor, int codigoCupom) throws PromocaoException, CupomException {
+        super(codigoProduto, novoValor);
+        if (codigoCupom <= 0) {
+            throw new CupomException("O código do cupom é inválido. Deve ser maior que 0.");
+        }
+
+        if (novoValor <= 0) {
+            throw new CupomException("O valor do cupom é inválido. Deve ser maior que 0.");
+        }
+        this.codigoCupom = codigoCupom;
     }
 
-    
-    
-    
+    public int getCodigoCupom() {
+        return codigoCupom;
+    }
+
 }
