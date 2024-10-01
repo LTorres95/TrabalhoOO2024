2f1956d6fa92c70a0aeb6a5a446d43eb2e5dd198
Gusta-1705 <gustavo.bem@estudante.ufjf.br>
2024-10-01 13:13:55 -0300

    adicionando atributos, construtor e as importações

--- src/main/java/com/mycompany/distribuidora/view/InterfaceGerente.java
+++ src/main/java/com/mycompany/distribuidora/view/InterfaceGerente.java
@@ -1,5 +1,23 @@
 package com.mycompany.distribuidora.view;
 
+import com.mycompany.distribuidora.model.Estoque.Produto;
+import com.mycompany.distribuidora.model.Usuarios.Vendedor;
+
+
+import javax.swing.*;
+import java.util.List;
+
 public class InterfaceGerente {
+    
+    private JFrame frame;
+    private DefaultListModel<Produto> produtoListModel;
+    private DefaultListModel<Vendedor> vendedorListModel;
+    private List<Produto> estoque;
+    private List<Vendedor> vendedores;
+
+    public InterfaceGerente(List<Produto> estoque, List<Vendedor> vendedores) {
+        this.estoque = estoque;
+        this.vendedores = vendedores;
+    }
 
 }
